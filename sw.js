a7d32bbcb76539a76d5c7ce8b3eb5eda0fdae8cb
cristianledesma2512-a11y <cristianledesma2512@gmail.com>
2026-04-09 13:07:13 +0000

    actualizacion

--- sw.js
+++ sw.js
@@ -4,3 +4,9 @@ self.options = {
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 10850126
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
